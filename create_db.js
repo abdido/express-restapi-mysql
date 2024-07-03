2cc97555e7cd93ea1362cd4e84712517dc270968
Abdi <sulistyawanar@gmail.com>
2024-07-04 04:13:16 +0800

    create db

---
+++ create_db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE express_restapi_mysql", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+    const sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  });
+});
